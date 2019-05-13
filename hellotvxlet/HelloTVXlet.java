30e5a81bba71e3a246b66c1126fa9e5153f6f489
Goan Hauquier <goan.hauquier@gmail.com>
2019-05-13 16:53:40 +0200

    spel werkt, enkele bugs

--- hellotvxlet/HelloTVXlet.java
+++ hellotvxlet/HelloTVXlet.java
@@ -2,6 +2,7 @@ package hellotvxlet;
 
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.MediaTracker;
 import javax.tv.xlet.*;
 import org.havi.ui.HIcon;
 import org.havi.ui.HScene;
@@ -14,13 +15,13 @@ import org.havi.ui.event.HTextListener;
 
 
 public class HelloTVXlet implements Xlet, HTextListener {
-Image imgs[]=new Image[4];
+Image imgs[]= new Image[6];
 String str[] = new String[]{"school", "film", "natuur", "kleur", "broadcast"};
 int woord = 0;
 String antwoord = str[woord];
 
 int huidig=0;
-      HIcon levels=new HIcon(imgs[huidig],170,30,360,360);
+      HIcon levels;
       
   HSinglelineEntry hst=new HSinglelineEntry(250,450,200,50,20);
     public HelloTVXlet() {
@@ -37,12 +38,24 @@ int huidig=0;
       hst.addHTextListener(this);
       scene.add(hst);
       scene.setVisible(true);
-      // foto 1
+      
+      // foto
        imgs[0]=scene.getToolkit().getImage("level1.png");
        imgs[1]=scene.getToolkit().getImage("level2.png");
-
+       imgs[2]=scene.getToolkit().getImage("level3.png");
+       imgs[3]=scene.getToolkit().getImage("level4.png");
+       imgs[4]=scene.getToolkit().getImage("level5.png");
+       imgs[5]=scene.getToolkit().getImage("gewonnen.png");
+       
+       MediaTracker mt=new MediaTracker(scene);
+       mt.addImage(imgs[0], 0);
+       mt.addImage(imgs[1], 1);
+       mt.addImage(imgs[2], 2);
+       mt.addImage(imgs[3], 3);
+       mt.addImage(imgs[4], 4);
+       mt.addImage(imgs[5], 5);
       
-      
+      levels=new HIcon(imgs[huidig],170,30,360,360);
       scene.add(levels);
 
       scene.validate();
@@ -68,14 +81,22 @@ int huidig=0;
 String guess = hst.getTextContent(HVisible.NORMAL_STATE).toString();
 System.out.println(guess);
 
-if (guess.equals(antwoord)) {
+if (woord >= 5) {
+        hst.setTextContent("Gewonnen", HVisible.NORMAL_STATE);
+} 
+
+if (guess.equals(antwoord)&& woord < 5) {
+    hst.setTextContent("", HVisible.NORMAL_STATE);
     System.out.println("juist");
-      levels.setGraphicContent(imgs[1], HVisible.NORMAL_STATE);
+      huidig = huidig+1;
+      levels.setGraphicContent(imgs[huidig], HVisible.NORMAL_STATE);
       woord = woord+1;
       antwoord = str[woord];
+         
 }
+ 
 
-    }
+ }
 
     public void caretMoved(HTextEvent arg0) {
  //       throw new UnsupportedOperationException("Not supported yet.");
